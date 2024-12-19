3606d1b323f75bef99de69a8afa5bca74c412f28
yashvarmora2306 <yashpatel23062002@gmail.com>
2024-12-19 12:26:14 +0530

    `Added new animations and styles for page2 and page3, updated script.js and style.css accordingly`

--- script.js
+++ script.js
@@ -37,4 +37,62 @@ function navAnimation() {
         })
     })
 }
+
+function page2Animation() {
+    var rightElems = document.querySelectorAll(".right-elem")
+
+    rightElems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            gsap.to(e.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            })
+        })
+        e.addEventListener("mouseleave", function () {
+
+            gsap.to(e.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            })
+        })
+        e.addEventListener("mousemove", function (dets) {
+            gsap.to(e.childNodes[3], {
+                x: dets.x - e.getBoundingClientRect().x - 50,
+                y: dets.y - e.getBoundingClientRect().y - 130,
+            })
+        })
+    })
+}
+
+function page3VideoAnimation() {
+
+    var page3Center = document.querySelector(".page3-center")
+    var video = document.querySelector("#page3 video")
+
+    page3Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+
+        })
+    })
+
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 1,
+            borderRadius: "30px"
+        })
+    })
+
+}
+
+
 navAnimation()
+
+page2Animation()
+
+page3VideoAnimation()
