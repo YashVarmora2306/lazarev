0beafe51237287d230e7166f3cd694b3d5444206
yashvarmora2306 <yashpatel23062002@gmail.com>
2024-12-19 19:07:49 +0530

    Implement locomotive and loading animations,page6 styles, and add hover effects for video elements

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+
+        // follwoing line is not required to work pinning on touch screen
+
+        /* pinType: document.querySelector(".smooth-scroll").style.transform
+          ? "transform"
+          : "fixed"*/
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+function loadingAnimation() {
+
+    var tl = gsap.timeline()
+    tl.from("#page1", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.2
+    })
+    tl.from("#page1", {
+        transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius: "150px",
+        duration: 2,
+        ease: "expo.out"
+    })
+    tl.from("nav", {
+        opacity: 0,
+        delay: -0.2
+    })
+    tl.from("#page1 h1, #page1 p, #page1 div", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.2
+    })
+}
+
 function navAnimation() {
     var nav = document.querySelector("nav")
 
@@ -88,11 +156,47 @@ function page3VideoAnimation() {
         })
     })
 
+    var sections = document.querySelectorAll(".sec-right")
+
+    sections.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            e.childNodes[3].style.opacity = 1
+            e.childNodes[3].play()
+        })
+        e.addEventListener("mouseleave", function () {
+            e.childNodes[3].style.opacity = 0
+            e.childNodes[3].load()
+        })
+    })
+
 }
 
+function page6Animations() {
+    gsap.from("#btm6-part2 h4", {
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#btm6-part2",
+            scroller: "#main",
+            // markers:true,
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true
+        }
+    })
+}
+
+
+
+
+locomotiveAnimation()
 
 navAnimation()
 
 page2Animation()
 
 page3VideoAnimation()
+
+page6Animations()
+
+loadingAnimation()
